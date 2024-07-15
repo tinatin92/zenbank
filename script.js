5a72ba1ada66fe643d549d13a865c6c94fc51345
tinatin92 <tinatygordadze@gmail.com>
2024-07-15 22:55:52 +0400

    slider

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        speed: 300,
+        Infinity:true,
+        arrows: false,
+        vertical: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        pauseOnFocus:false,
+        pauseOnHover:false,
+    });
+  });
